05207eb1d786d0573e0713ace1a24a28a02ffdc9
Marcio-Brust <brustmw21@gmail.com>
2022-10-19 01:13:55 -0300

    scroll e outside

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,10 +1,19 @@
+import outsideClick from "./outsideclick.js";
+
 export default function initiMenuMobile() {
   const button = document.querySelector(".btn-mobile");
 
   function toggleMenu(event) {
-    if (event.type === "touchstart") event.preventDefault();
     const nav = document.querySelector(".nav");
-    nav.classList.toggle("active");
+    if (event.type === "touchstart") event.preventDefault();
+    const ativo = nav.classList.contains("active");
+    nav.classList.remove("active");
+    outsideClick(button, ["click", "touchstart"], () => {
+      nav.classList.remove("active");
+    });
+    if (!ativo) {
+      nav.classList.add("active");
+    }
   }
 
   button.addEventListener("click", toggleMenu);
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 import initiMenuMobile from "./modules/menu-mobile.js";
+import initScrollSuave from "./modules/scroll-suave.js";
 
 initiMenuMobile();
+initScrollSuave();
